89603df41877a6c2072125053511b8a70d1229b4
Ankit Dimri <ankitdimri1999@gmail.com>
2020-03-29 00:23:06 +0530

    re ranked the nodes

--- CP-structure.R
+++ CP-structure.R
@@ -11,13 +11,13 @@ cp_set <- function (G, beta) {
   # Rank the nodes
   U = c()
   U$nodes = rerank_nodes (G)
-  k = (2 * length (E(G))) / length (V(G))
+  #k = (2 * length (E(G))) / length (V(G))
+  print (U$nodes)
+  #for (i in 1:length (U))
+   # U$rd = compute_RD (U$nodes, G)
   
-  for (i in 1:length (U))
-    U$rd = compute_RD (U$nodes, G)
-  
-  Cset = FindCoreSet (G, U, beta)
-  CPset = FindCPSet (G, Cset, NumC)
+  #Cset = FindCoreSet (G, U, beta)
+  #CPset = FindCPSet (G, Cset, NumC)
 }
 
 
@@ -26,7 +26,7 @@ rerank_nodes <- function (lg) {
   # Rank the given nodes
   # Use any centrality measure for finding the first node; we used: betweeness
   U = c()
-  U$n = c (which.max (igraph::betweeness (lg)))
+  U$n = c (which.max (igraph::betweenness (lg)))
   count = 1
   maxD = max (igraph::degree (lg))
   # Populate neighbours group
@@ -47,9 +47,13 @@ rerank_nodes <- function (lg) {
     # remove from neighbours group
     ng$n = ng$n [ng$n != add_node]
     # add its neighbours to the group
-    ng$n = c (ng$n, neighbors (lg, add_node))
+    n = neighbors (lg, add_node)
+    add_n = n [! n %in% intersect (n, U$n)]
+    ng$n = c (ng$n, add_n)
+    count = count + 1
     
   }
   
   return (U$n)
 }
+
