70376d815372a36846439db7932a4e595556e710
Ankit Dimri <ankitdimri1999@gmail.com>
2020-03-28 20:43:52 +0530

    intermittent code

--- CP-structure.R
+++ CP-structure.R
@@ -18,3 +18,34 @@ cp_set <- function (G, beta) {
   Cset = FindCoreSet (G, U, beta)
   CPset = FindCPSet (G, Cset, NumC)
 }
+
+
+rerank_nodes <- function (lg) {
+  
+  # Rank the given nodes
+  # Use any centrality measure for finding the first node; we used: betweeness
+  U = c (which.max (igraph::betweeness (lg)))
+  count = 1
+  # Populate neighbours group
+  ng = c(neighbors (lg, U [1]))
+  
+  while (count != length (V(lg))) {
+    
+    for (i in 1:length (ng)) {
+      
+    }
+    
+    # repopulate neighbours group
+    for (i in 1:length (U)) {
+      n = neighbors (lg, U [i])
+      for (j in 1:length (n)) {
+        if (!(n [i] %in% U)) {
+          ng = c (ng, n)
+        } 
+      }
+    }
+    
+    
+  }
+  
+}
